019f7b3221ae47bbf7a471cb863bf12ee9fc3112
lingalaabhi34 <122089325+lingalaabhi34@users.noreply.github.com>
2023-10-30 11:46:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 //your code here!
 
+const scrollContainer = document.querySelector("#infi-list");
+let elementNumber = 1;
+function addTenElements(){
+    for(let i=1;i<=10;i++){
+        const li = document.createElement("li");
+        li.innerText = `Item ${
+            elementNumber++
+        }`;
+        scrollContainer.appendChild(li);
+    }
+
+}
+
+
+scrollContainer.addEventListener("scroll",()=>{
+    let totalScrollableHeight =scrollContainer.scrollHeight;
+    let visibleHeight = scrollContainer.clientHeight;
+    let scrolledHeight = scrollContainer.scrollTop;
+    let total = totalScrollableHeight-visibleHeight-scrolledHeight;
+    if(total<=4){
+        addTenElements();
+    }
+})
+
+addTenElements();
